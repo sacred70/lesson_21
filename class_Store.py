5d6565ac5f1ec32825ed588678f3b64be626bca9
sacred70 <sacred70@mail.ru>
2023-03-14 18:42:35 +0700

    class Store

--- class_Store.py
+++ class_Store.py
@@ -2,9 +2,9 @@
 Шаг 2. Реализуйте класс Store. В нем хранится любое количество любых товаров.
 """
 
-from class_Storage import Storage  # импортируем родительский абстрактный класс
+from class_Storage import Storage  # импортируем родительский  класс
 
 
-class Store(Storage):  # создаем класс наследуемый от абстрактного класса Storage
+class Store(Storage):  # создаем класс наследуемый от родительского класса Storage
     def __init__(self, items: dict, quantity: int = 100):
-        super().__init__(items, quantity)  # наследуем поля из абстрактного класса
+        super().__init__(items, quantity)  # наследуем поля из родительского класса
